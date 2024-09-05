ecf7b3201be7f541f050f75ccc616740c55c4264
pman <pman-$-432894328923@gmail.com>
2024-09-05 13:55:33 +0200

    ...

--- coredns/run.sh
+++ coredns/run.sh
@@ -2,8 +2,3 @@
 
 # run coredns
 docker-compose up -d
-
-# query our server from a client in the same subnet or from the server directly
-# docker by default uses bridge networking - a private virtual network @172.*.*.* on the docker host
-DNS_SERVER_IP=$(docker inspect coredns | jq -r '.[0].NetworkSettings.Networks.coredns_default.IPAddress')
-dig @"$DNS_SERVER_IP" host.example.com
