9c8b44dd63d2a281c43236cc3f654ac7864749c3
pman <pman-$-432894328923@gmail.com>
2024-09-06 00:57:29 +0200

    ...

--- run.sh
+++ run.sh
@@ -55,37 +55,40 @@ sleep 5
 
 cd coredns
 # configure corefile to use variable nodeport
-NODE_PORT=$(kubectl get svc pihole-nodeport -o jsonpath='{.spec.ports[0].nodePort}')
-NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
-sed \
-  -e "s/NODE_PORT/$NODE_PORT/g" \
-  -e "s/NODE_IP/$NODE_IP/g" \
-  ./Corefile.template > ./Corefile
-# run coredns
-docker-compose up -d
-cd ..
+# NODE_PORT=$(kubectl get svc pihole-nodeport -o jsonpath='{.spec.ports[0].nodePort}')
+# NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
+# sed \
+#   -e "s/NODE_PORT/$NODE_PORT/g" \
+#   -e "s/NODE_IP/$NODE_IP/g" \
+#   ./Corefile.template > ./Corefile
+# # run coredns
+# docker-compose up -d
+# cd ..
+#
+# sleep 5
+#
+# # query our server from a client in the same subnet or from the server directly
+# # docker by default uses bridge networking - a private virtual network @172.*.*.* on the docker host
+#
+# TEST_FQDN=nginx.example.org
+# DNS_SERVER_IP=$(docker inspect coredns | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
+# dig @"$DNS_SERVER_IP" "$TEST_FQDN"
+#
+#
+# # temporarily override local DNS /etc/resolve.conf
+# if [ -f /etc/resolv.conf.bkp ]; then
+#   echo "/etc/resolv.conf.bkp already exists!"
+#   exit 1
+# else
+#   sudo cp /etc/resolv.conf /etc/resolv.conf.bkp
+#   echo "nameserver $DNS_SERVER_IP" | sudo tee /etc/resolv.conf
+# fi
+#
+#
+# dig @"$NODE_IP" -p "$NODE_PORT" "$TEST_FQDN"
+#
+#
+# kubectl run migrate -it --rm --restart=Never --image=nicolaka/netshoot -- /bin/sh -c 'dig @pihole.default.svc.cluster.local nginx.example.org'
 
-sleep 5
-
-# query our server from a client in the same subnet or from the server directly
-# docker by default uses bridge networking - a private virtual network @172.*.*.* on the docker host
-
-TEST_FQDN=nginx.example.org
-DNS_SERVER_IP=$(docker inspect coredns | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
-dig @"$DNS_SERVER_IP" "$TEST_FQDN"
-
-
-# temporarily override local DNS /etc/resolve.conf
-if [ -f /etc/resolv.conf.bkp ]; then
-  echo "/etc/resolv.conf.bkp already exists!"
-  exit 1
-else
-  sudo cp /etc/resolv.conf /etc/resolv.conf.bkp
-  echo "nameserver $DNS_SERVER_IP" | sudo tee /etc/resolv.conf
-fi
-
-
-dig @"$NODE_IP" -p "$NODE_PORT" "$TEST_FQDN"
-
-
-kubectl run migrate -it --rm --restart=Never --image=nicolaka/netshoot -- /bin/sh -c 'dig @pihole.default.svc.cluster.local nginx.example.org'
+nslookup nginx.example.org
+dig nginx.example.org
