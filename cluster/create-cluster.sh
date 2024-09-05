d5de45332c4958b69c4d461c26d2296a42198627
pman <pman-$-432894328923@gmail.com>
2024-09-05 16:41:16 +0200

    ...

---
+++ cluster/create-cluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kind delete cluster && kind create cluster --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+  - role: worker
+  - role: worker
+EOF
+kubectl cluster-info --context kind-kind
