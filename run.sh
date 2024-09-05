412170f5488277c8f12f2c70f9b2b0353e4542ed
pman <pman-$-432894328923@gmail.com>
2024-09-05 18:30:06 +0200

    chore: update file(s): create-cluster.sh run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,30 @@
 
 set -eux
 
-workdir="$(pwd)"
+kind delete cluster && kind create cluster --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+  - role: worker
+  - role: worker
+EOF
+kubectl cluster-info --context kind-kind
 
-"$workdir"/cluster/create-cluster.sh
+kustomize build .cluster/manifests/ | kubectl apply -f -
 
-"$workdir"/coredns/create-dns.sh
+# configure corefile to use variable nodeport
+NODE_PORT=$(kubectl get svc pihole -o jsonpath='{.spec.ports[0].nodePort}')
+NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
+sed \
+  -e "s/NODE_PORT/$NODE_PORT/g" \
+  -e "s/NODE_IP/$NODE_IP/g" \
+  ./Corefile.template > ./Corefile
 
+# run coredns
+docker-compose up -d
+
+# query our server from a client in the same subnet or from the server directly
+# docker by default uses bridge networking - a private virtual network @172.*.*.* on the docker host
+DNS_SERVER_IP=$(docker inspect coredns | jq -r '.[0].NetworkSettings.Networks.coredns_default.IPAddress')
+dig @"$DNS_SERVER_IP" host.example.com
