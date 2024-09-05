dcdf4116bd25f92e6d5f04e6a1250c4fb8ac1041
pman <pman-$-432894328923@gmail.com>
2024-09-05 20:02:24 +0200

    ...

--- delete.sh
+++ delete.sh
@@ -5,6 +5,8 @@ set -eux
 if [ -f /etc/resolv.conf.bkp ]; then
   sudo cp /etc/resolv.conf.bkp /etc/resolv.conf
   sudo rm /etc/resolv.conf.bkp
+else
+  : no /etc/resolv.conf.bkp found
 fi
 
 cd coredns && docker-compose down
