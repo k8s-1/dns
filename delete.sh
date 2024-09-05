8749b8499db9c95b32c2a27bb1e604967b1434d0
pman <pman-$-432894328923@gmail.com>
2024-09-05 19:34:24 +0200

    ...

--- delete.sh
+++ delete.sh
@@ -2,7 +2,10 @@
 
 set -eux
 
-cp /etc/resolv.conf.bkp /etc/resolv.conf
+if [ -f /etc/resolv.conf.bkp ]; then
+  cp /etc/resolv.conf.bkp /etc/resolv.conf
+  rm /etc/resolv.conf.bkp
+fi
 
 cd coredns && docker-compose down
 cd - || exit
