715957c7838ac7a2953d5485fe9d097a6c60da3d
pman <pman-$-432894328923@gmail.com>
2024-09-06 17:26:44 +0200

    ...

--- cluster/tls/create-certs.sh
+++ cluster/tls/create-certs.sh
@@ -6,7 +6,6 @@
 
 # instead use a CA to validate identity of public crt, mkcert uses a locally hosted CA
 mkcert -install
-# mkcert -uninstall
 mkcert nginx.example.org # create key + pem
 openssl x509 -in nginx.example.org.pem -text -noout
 kubectl create secret tls my-demo-app-tls --key nginx.example.org-key.pem --cert nginx.example.org.pem $dry > tls-secret.yaml
