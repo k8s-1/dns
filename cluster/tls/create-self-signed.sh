13a1eeb611c140f9a19a6d4cd9f24e51a77aeaba
pman <pman-$-432894328923@gmail.com>
2024-09-06 12:53:13 +0200

    ...

--- cluster/tls/create-self-signed.sh
+++ cluster/tls/create-self-signed.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 # create self-signed tls certificate
-
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=my-demo-app.local"
+
+# store in secret
+kubectl create secret tls my-demo-app-tls --key tls.key --cert tls.crt --dry-run=client -oyaml > tls-secret.yaml
