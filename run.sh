89a7be04548456ac79dcf0c8544569a288e6727e
pman <pman-$-432894328923@gmail.com>
2024-09-10 00:09:52 +0200

    ...

--- run.sh
+++ run.sh
@@ -49,7 +49,7 @@ sleep 60
 
 # cert-manager
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
-sleep 5
+sleep 20
 kubectl apply -f - <<EOF
 apiVersion: cert-manager.io/v1
 kind: Issuer
