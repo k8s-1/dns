1039a329004f8162db953fead161c3e45e5c3ae8
pman <pman-$-432894328923@gmail.com>
2024-09-09 23:45:46 +0200

    ...

--- run.sh
+++ run.sh
@@ -47,6 +47,10 @@ kustomize build ./infra/ | kubectl apply -f -
 sleep 60
 
 
+# cert-manager
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
+
+
 # traefik ingress-controller
 kustomize build ./traefik/ | kubectl apply -f -
 
