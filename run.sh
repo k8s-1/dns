3370e249d217431060f1b8e8e0cfdf67797ccc75
pman <pman-$-432894328923@gmail.com>
2024-09-09 23:58:51 +0200

    ...

--- run.sh
+++ run.sh
@@ -49,6 +49,27 @@ sleep 60
 
 # cert-manager
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
+cat <<EOF> kubectl apply -f -
+apiVersion: cert-manager.io/v1
+kind: Issuer
+metadata:
+  name: letsencrypt-staging
+spec:
+  acme:
+    # The ACME server URL
+    server: https://acme-staging-v02.api.letsencrypt.org/directory
+    # Email address used for ACME registration
+    email: user@example.com
+    # Name of a secret used to store the ACME account private key
+    privateKeySecretRef:
+      name: letsencrypt-staging
+    # Enable the HTTP-01 challenge provider
+    solvers:
+      - http01:
+          ingress:
+            ingressClassName: traefik
+EOF
+
 
 
 # traefik ingress-controller
